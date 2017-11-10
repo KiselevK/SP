472847c8b962d311670d87ea8da1295d32c4e800
KiselevK <kirill.kiselev220@gmail.com>
2017-11-10 20:11:25 +0200

    Added project solution

---
+++ VSproject/SPprojectV1/SPui/SPui/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MainLogicLayer;
+namespace SPui.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var test = new Test();
+            ViewData["HelloWorld"] = test.HelloWorld;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ VSproject/SPprojectV1/SPui/SPui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SPui.Startup))]
+namespace SPui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
